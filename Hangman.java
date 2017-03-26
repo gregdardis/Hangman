f2cb2f57d92f53e23750ad3c17c7038fd73250bd
Greg Dardis <dardisgreg@gmail.com>
2017-03-26 16:33:09 -0600

    added some methods, cleaned up main method

--- Hangman.java
+++ Hangman.java
@@ -9,26 +9,30 @@ public class Hangman {
 	int numberOfMisses = 0;
 	int numberOfMissesAllowed = 8;
 	char[] letterMisses = new char[numberOfMissesAllowed];
-	
+	char[] theRandomWord;
+	char[] notGuessedArray;
+
 	public void chooseRandomWord() {
 		String[] randomWords = {"helicoptor", "library", "incapacitate"};
 		int numberOfChoices = randomWords.length;
-		
+
 		int randomNumber = rng.nextInt(numberOfChoices);
 		randomWord = randomWords[randomNumber];
-		
+		theRandomWord = new char[randomWord.length()];
 	}
-	
+
 	public void askForALetter() {
+		System.out.print("Guess: ");
 		guessedLetter = myScanner.next().charAt(0);
 	}
-	
+
 	public void searchForThatLetter() {
 		boolean wasItFound = false;
 		for (int i = 0; i < randomWord.length(); i++) {
 			if (guessedLetter == randomWord.charAt(i)) {
 				wasItFound = true;
 				System.out.println("The letter was found");
+				notGuessedArray[i] = guessedLetter;
 			}
 		}
 		if (wasItFound == false) {
@@ -42,35 +46,55 @@ public class Hangman {
 			}
 		}
 	}
-	
+	// make an array of chars with the letters of the randomWord string
+	public void randomWordToArray() {
+		for (int i = 0; i < randomWord.length(); i++) {
+			theRandomWord[i] = randomWord.charAt(i);
+		}
+	}
+	// make an array of chars showing their correct guesses, otherwise
+	// having an underscore in that spot
+	public void initializeGuessesArray() {
+		notGuessedArray = new char[randomWord.length()];
+		for (int i = 0; i < randomWord.length(); i++) {
+			notGuessedArray[i] = '_';
+			//System.out.print(notGuessedArray[i] + " ");
+		}
+	}
+
+	public void correctGuessesArray() {
+		for (int i = 0; i < randomWord.length(); i++) {
+			System.out.print(notGuessedArray[i] + " ");
+			//System.out.print(notGuessedArray[i] + " ");
+		}
+	}
+
+	public void printMisses() {
+		System.out.print("Misses: ");
+		for (int i = 0; i < numberOfMisses; i++) {
+			System.out.print(letterMisses[i] + " ");
+		}
+		System.out.print("\n");
+	}
+
 	public void playGame() {
 		chooseRandomWord();
-		System.out.println(randomWord);
-		
-		System.out.print("Word: ");
-		System.out.println("\n");
-		System.out.print("Misses: "); 
-		for (int i = 0; i < numberOfMisses; i++) {
-			System.out.print(letterMisses[i] + " ");
+		randomWordToArray();
+		initializeGuessesArray();
+		while (true) {
+			askForALetter();
+			searchForThatLetter();
+			correctGuessesArray();
 		}
-		System.out.println("\n");
-		System.out.print("Guess: ");
-		askForALetter();
-		searchForThatLetter();
-		System.out.print("Misses: "); 
-		for (int i = 0; i < numberOfMisses; i++) {
-			System.out.print(letterMisses[i] + " ");
-		}
-		
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		Hangman hangmanGame = new Hangman();
-		
+
 		hangmanGame.playGame();
 	}
 
 }
-// TODO: MAKE A PRINT MISSES METHOD TO REPLACE FOR LOOP IN PLAYGAME
+
 // REARRANGE PLAYGAME SO SHIT HAPPENS IN THE RIGHT ORDER
